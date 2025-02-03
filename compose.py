f20a2faec51832d09b53ffce214f91c3e6b76db8
dary-dc <crespoydelgado@gmail.com>
2025-02-02 22:47:55 -0500

    Ready to generate fun text

---
+++ compose.py
@@ -0,0 +1,62 @@
+from reader import get_words_from_file
+from graph import Graph
+
+# add the words to the graph
+def make_graph(words):
+    g = Graph()
+    # first we create a vertex object and add it to our Graph Class, with the word as the value if it doesn't still exists
+    previous_vertex = g.add_vertex(words[0]) # if we process only one text the first word won't be processed
+    for word in words[1:]: # for each word in words, except the very first one
+        curr_vertex = g.get_vertex(word) # let's check if the word is a vertex in the graph already
+        if curr_vertex is None: # if it isn't
+            curr_vertex = g.add_vertex(word) # we add it to our graph as a new vertex
+            previous_vertex.add_edge_to(curr_vertex) # if the word wasn't a vertex in our graph, we obviously need to connect it with an edge with the previous vertex
+        else: # if it's already a vertex
+            if curr_vertex in previous_vertex.adjacent.keys(): # let's check if it is an edjacent vertex
+                previous_vertex.increment_edge(curr_vertex)
+            else:
+                previous_vertex.add_edge_to(curr_vertex)
+        previous_vertex = curr_vertex
+    return g
+
+def visualize_data(g, words, all_vertices=False):
+    print("number of words processed:", len(words))
+    print("number of words in the graph (number of unique words):", len(g.get_words()), "\n")
+    if all_vertices:
+        for vertex in g.vertices.values():
+            print(f"'{vertex.value}':", vertex.get_adjacent_words_and_weights())
+    else: # only vertices with weight greater than zero
+        for vertex in g.vertices.values():
+            if any(vertex.adjacent.values()):
+                print(f"'{vertex.value}':", vertex.get_adjacent_words_and_weights())
+
+def compose(g, starting_word=None, length=100):
+    """
+    Creates the composition from the input data based on the Markov chain model.
+    Args:
+        g (Graph): Graph with the data.
+        starting_word (boolean): inicial word of the composition (will determine the composition itself, according to the model)
+        length (int): length od the composition (length == len(compose(g).split()))
+    Returns:
+        string: composition based on the most likely sucession of words. If any words repeats (which will cause repetition in the composition), we go to the word with the second greatest weight, then the third and so on.
+    """
+    if not starting_word:
+        starting_word = list(g.get_words())[0]
+    
+    composition = starting_word + " "
+    for i in range(length-1):
+        curr_word = g.get_next_word(starting_word).value
+        composition += curr_word + " "
+        starting_word = curr_word
+        
+    return composition
+
+def main():
+    text_file_path = "texts/Song of Myself (1892 version) by Walt Whitman _ Poetry Foundation.pdf"
+    words = get_words_from_file(text_file_path)
+
+    g = make_graph(words)
+    print(compose(g, "myself"))
+
+if __name__ == "__main__":
+    main()
---
+++ graph.py
@@ -0,0 +1,95 @@
+""" Markov Chain is basically a mathematical probabilistic model
+in which the current state is determined by the the previous one.
+We used this model to generate text. By adding a weight to each word
+that we found in a sample text, we can determine the most likely
+succession of words.
+"""
+# this is our Markov Chain representation
+# define the graph in terms of vertices
+class Vertex:
+    def __init__(self, value):
+        self.value = value # value is just the word!!!
+        self.adjacent = {} # keys are the adjacent vertices, values are its weights.
+        # for easily accessing to an adjacent vertex weight (e.g., for weight incrementation)
+
+    # add an edge (a connection) to a vertex
+    def add_edge_to(self, vertex, weight=0):
+        self.adjacent[vertex] = weight
+
+    # increment the weight of the edge in vertex
+    # (since it defaults to '0' if 'vertex' is not found
+    # it can create a new vertex with of weight = 1)
+    def increment_edge(self, vertex):
+        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1 # actually it should have been: self.adjacent.get(vertex) + 1, but we don't want an accidental error to occur
+
+    def get_adjacent_words_and_weights(self):
+        return [(vertex.value, weight) for vertex, weight in self.adjacent.items()]
+
+from random import choice
+# create a graph for storing those vertices
+# the graph is an important data structure for this project since it will allow us to have in a unique container all the words we process.
+class Graph:
+    def __init__(self):
+        self.vertices = {} # key is the word, value is the Vertex object
+
+    # return all the words
+    def get_words(self):
+        return self.vertices.keys()
+
+    # for contructing the graph
+    def add_vertex(self, value):
+        self.vertices[value] = Vertex(value)
+        return self.vertices[value]
+
+    # search for a vertex in the graph given the word
+    def get_vertex(self, value):
+        return self.vertices.get(value, None)
+
+    # search for a vertex in the graph given the word (for users)
+    def get_vertex_user(self, value):
+        vertex = self.vertices.get(value, None)
+        if vertex: # if the value/word is associated to a vertex we simply return the vertex object
+            return vertex
+        # otherwise we ask for the user input to know if the creation of a new vertex is desired
+        i = 0
+        while True:
+            answer = input(f"The value {value} isn't associated with a vertex. \
+                           \nDo you want to create a new vertex with this word? (y/n): \
+                           \n")
+            if answer == "n":
+                return
+            elif answer == "y":
+                break
+            else:
+                print("That isn't a valid input. Please, try it again.")
+            if i == 10:
+                return
+            i += 1
+        # if the user chooses to create a new vertex with the entered value 
+        self.add_vertex(value)
+        vertex = self.vertices[value]
+        return vertex            
+
+    # search for the most probable word linked to the current word or to the current vertex
+    def get_next_word(self, word, vertex=None):
+        current_value = word
+        current_vertex = vertex
+        # if the vertex wasn't provided as input
+        if current_value and current_vertex is None: # search for the corresponding vertex value the vertex 
+            current_vertex = self.get_vertex(current_value) # with our previously defined method
+        # once we have the vertex, we need to check which of its adjacents vertices has the greatest value (i.e., the greatest weight). That would be the most probable word to be next
+        if current_vertex: # We check if there's a current_vertex bcuz it exists the possibility that the user has entered a wrong word and doesn't want to add it
+            if list(current_vertex.adjacent):
+                greatest_weight = -1 # remember the weights are initialized with 0 as weight
+                for word, weight in current_vertex.adjacent.items():
+                    if weight > greatest_weight:
+                        probable_word = word
+                del current_vertex.adjacent[probable_word] # destructively iterate over the dictionary to avoid word repetition in the composition
+                return probable_word
+            else:# if a word adjacent dictionary gets empty
+                return self.vertices[choice(list(self.vertices))] # set a random word to continue the composition. This will cause the determinism to be lost, but we don't want to complicate more this project
+        # if the word entered wasn't in the graph
+        if input(f"We couldn't find the word '{current_value}' in the graph. Would you like to try with another word? (y/n):\n") == "y":
+            word = input("Type the word: ")
+            return self.get_next_word(word)
+            # print(word)
